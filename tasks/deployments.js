625af6433f95880d19b181a5b17ef6f71325aeef
Nico Prat <nicooprat@gmail.com>
2016-09-16 15:28:05 +0200

    Update warning message string

--- tasks/deployments.js
+++ tasks/deployments.js
@@ -180,10 +180,6 @@ module.exports = function(grunt) {
 
         grunt.file.mkdir(output_paths.dir);
 
-        // 1) Get array of tables to ignore, as defined in the config, and format it correctly
-        if( config.ignoreTables ) {
-            var ignoreTables = '--ignore-table=' + config.database + "." + config.ignoreTables.join(' --ignore-table='+config.database+'.');
-        }        
 
         // 2) Compile MYSQL cmd via Lo-Dash template string
         var tpl_mysqldump = grunt.template.process(tpls.mysqldump, {
@@ -191,13 +187,12 @@ module.exports = function(grunt) {
                 user: config.user,
                 pass: config.pass,
                 database: config.database,
-                host: config.host,
-                ignoreTables: ignoreTables || ''
+                host: config.host
             }
         });
 
-        // 3) Test whether MYSQL DB is local or whether requires remote access via SSH
 
+        // 3) Test whether MYSQL DB is local or whether requires remote access via SSH
         if (typeof config.ssh_host === "undefined") { // it's a local connection
             grunt.log.writeln("Creating DUMP of local database");
             cmd = tpl_mysqldump;
@@ -214,7 +209,7 @@ module.exports = function(grunt) {
         }
 
         // Capture output...
-        var output = shell.exec(cmd, {silent: true}).output;
+        var output = shell.exec(cmd, {silent: true}).output.replace( 'Warningmysqldump: [Warning] Using a password on the command line interface can be insecure.', '' );
 
         // Write output to file using native Grunt methods
         grunt.file.write( output_paths.file, output );
@@ -254,7 +249,7 @@ module.exports = function(grunt) {
 
         search_replace: "sed -i '' 's#<%= search %>#<%= replace %>#g' <%= path %>",
 
-        mysqldump: "mysqldump -h <%= host %> -u<%= user %> -p<%= pass %> <%= database %> <%= ignoreTables %>",
+        mysqldump: "mysqldump -h <%= host %> -u<%= user %> -p<%= pass %> <%= database %>",
 
         mysql: "mysql -h <%= host %> -u <%= user %> -p<%= pass %> <%= database %>",
 
@@ -264,7 +259,6 @@ module.exports = function(grunt) {
 
 
 };
-
 
 
 
